760a9031331351b596e2bfa328b85535302f0ed4
Viktoria Stoyanova <ViktoriaWD@hotmail.com>
2024-02-13 13:04:04 +0000

    setting up a new project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{html,js}'],
+  content: ['./**/*.html', './js/**/*.js'],
   theme: {
     screens: {
       xs: '376px',
@@ -12,20 +12,34 @@ module.exports = {
     },
     extend: {
       colors: {
-        'primary': '#FF6363',
-        'secondary': {
-          100: '#E2E2D5',
-          200: '#888883',
-        },
+        brightBlue: 'hsl(220, 98%, 61%)', // Main Background: hsl(220, 98%, 61%)
+        checkBackgroundLinearGgradient1: 'hsl(192, 100%, 67%)',
+        checkBackgroundLinearGgradient2:  'hsl(280, 87%, 65%)',
+        //light theme
+        veryLightGray: 'hsl(0, 0%, 98%)',
+        veryLightGrayishBlue: 'hsl(236, 33%, 92%)',
+        lightGrayishBlue: 'hsl(233, 11%, 84%)',
+        darkGrayishBlue: 'hsl(236, 9%, 61%)',
+        veryDarkGrayishBlue: 'hsl(235, 19%, 35%)',
+        //dark theme
+        veryDarkBlue: 'hsl(235, 21%, 11%)',
+        veryDarkDesaturatedBlue: 'hsl(235, 24%, 19%)',
+        lightGrayishBlue: 'hsl(234, 39%, 85%)',
+        lightGrayishBlueHover: 'hsl(236, 33%, 92%)',
+        darkGrayishBlue: 'hsl(234, 11%, 52%)',
+        veryDarkGrayishBlue: 'hsl(233, 14%, 35%)',
+        veryDarkGrayishBlue: 'hsl(237, 14%, 26%)',
+    
       },
       fontSize: {
-        inputs: '24px',
+        inputs: '18px',
       },
       fontFamily: {
-        body: ['Space Mono', 'monospace'],
+        body: ['Josefin Sans', 'sans-serif'],
       },
       fontWeight: {
         700: '700',
+        400: '400',
       },
     },
   },
